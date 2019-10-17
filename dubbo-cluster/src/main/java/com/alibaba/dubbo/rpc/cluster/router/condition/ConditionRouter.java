b8dcd62211622ea347da89c4bcc5611a09ee5b84
lancer.yao <lancer.yao@gaiaworks.cn>
2019-10-17 14:54:31 +0800

    10-16 集群容错-----route  服务消费和服务提供url进行匹配

--- dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/router/condition/ConditionRouter.java
+++ dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/router/condition/ConditionRouter.java
@@ -41,6 +41,7 @@ import java.util.regex.Pattern;
 
 /**
  * ConditionRouter
+ * 条件路由  host = 10.20.153.10 => host = 10.20.153.11
  */
 public class ConditionRouter extends AbstractRouter {
 
@@ -51,22 +52,34 @@ public class ConditionRouter extends AbstractRouter {
     private final Map<String, MatchPair> whenCondition;
     private final Map<String, MatchPair> thenCondition;
 
+    /**
+     * 预先对路由规则做预处理
+     * 然后调用 parseRule 方法分别对服务提供者和消费者规则进行解析，
+     * 最后将解析结果赋值给 whenCondition 和 thenCondition 成员变量。
+     */
     public ConditionRouter(URL url) {
         this.url = url;
+        // 获取 priority 和 force 配置
         this.priority = url.getParameter(Constants.PRIORITY_KEY, DEFAULT_PRIORITY);
         this.force = url.getParameter(Constants.FORCE_KEY, false);
         try {
+            // 获取路由规则
             String rule = url.getParameterAndDecoded(Constants.RULE_KEY);
             if (rule == null || rule.trim().length() == 0) {
                 throw new IllegalArgumentException("Illegal route rule!");
             }
             rule = rule.replace("consumer.", "").replace("provider.", "");
+            // 定位 => 分隔符
             int i = rule.indexOf("=>");
+            // 分别获取服务消费者和提供者匹配规则
             String whenRule = i < 0 ? null : rule.substring(0, i).trim();
             String thenRule = i < 0 ? rule.trim() : rule.substring(i + 2).trim();
+            // 解析服务消费者匹配规则
             Map<String, MatchPair> when = StringUtils.isBlank(whenRule) || "true".equals(whenRule) ? new HashMap<String, MatchPair>() : parseRule(whenRule);
+            // 解析服务提供者匹配规则
             Map<String, MatchPair> then = StringUtils.isBlank(thenRule) || "false".equals(thenRule) ? null : parseRule(thenRule);
             // NOTE: It should be determined on the business level whether the `When condition` can be empty or not.
+            // 将解析出的匹配规则分别赋值给 whenCondition 和 thenCondition 成员变量
             this.whenCondition = when;
             this.thenCondition = then;
         } catch (ParseException e) {
@@ -74,8 +87,13 @@ public class ConditionRouter extends AbstractRouter {
         }
     }
 
+    /**
+     * 对服务提供者和消费者规则进行解析
+     * 逻辑就是：由正则表达式和一个while循环以及数个条件分钟组成。
+     */
     private static Map<String, MatchPair> parseRule(String rule)
             throws ParseException {
+        // 定义条件映射集合
         Map<String, MatchPair> condition = new HashMap<String, MatchPair>();
         if (StringUtils.isBlank(rule)) {
             return condition;
@@ -84,18 +102,50 @@ public class ConditionRouter extends AbstractRouter {
         MatchPair pair = null;
         // Multiple values
         Set<String> values = null;
+
+        // 通过正则表达式匹配路由规则，ROUTE_PATTERN = ([&!=,]*)\s*([^&!=,\s]+)
+        // 这个表达式看起来不是很好理解，第一个括号内的表达式用于匹配"&", "!", "=" 和 "," 等符号。
+        // 第二括号内的用于匹配英文字母，数字等字符。举个例子说明一下：
+        //    host = 2.2.2.2 & host != 1.1.1.1 & method = hello
+        // 匹配结果如下：
+        //     括号一      括号二
+        // 1.  null       host
+        // 2.   =         2.2.2.2
+        // 3.   &         host
+        // 4.   !=        1.1.1.1
+        // 5.   &         method
+        // 6.   =         hello
+
+        //循环结束 {
+        //    "host": {
+        //        "matches": ["2.2.2.2"],
+        //        "mismatches": ["1.1.1.1"]
+        //    },
+        //    "method": {
+        //        "matches": ["hello"],
+        //        "mismatches": []
+        //    }
+        //}
         final Matcher matcher = ROUTE_PATTERN.matcher(rule);
         while (matcher.find()) { // Try to match one by one
+            // 获取括号一内的匹配结果
             String separator = matcher.group(1);
+            // 获取括号二内的匹配结果
             String content = matcher.group(2);
             // Start part of the condition expression.
+            // 分隔符为空，表示匹配的是表达式的开始部分
             if (separator == null || separator.length() == 0) {
+                // 创建 MatchPair 对象
                 pair = new MatchPair();
+                // 存储 <匹配项, MatchPair> 键值对，比如 <host, MatchPair>
                 condition.put(content, pair);
             }
             // The KV part of the condition expression
+            // 如果分隔符为 &，表明接下来也是一个条件
             else if ("&".equals(separator)) {
+                // 尝试从 condition 获取 MatchPair
                 if (condition.get(content) == null) {
+                    // 未获取到 MatchPair，重新创建一个，并放入 condition 中
                     pair = new MatchPair();
                     condition.put(content, pair);
                 } else {
@@ -103,6 +153,7 @@ public class ConditionRouter extends AbstractRouter {
                 }
             }
             // The Value in the KV part.
+            // 分隔符为 =
             else if ("=".equals(separator)) {
                 if (pair == null)
                     throw new ParseException("Illegal route rule \""
@@ -111,9 +162,11 @@ public class ConditionRouter extends AbstractRouter {
                             + content + "\".", matcher.start());
 
                 values = pair.matches;
+                // 将 content 存入到 MatchPair 的 matches 集合中
                 values.add(content);
             }
             // The Value in the KV part.
+            //分隔符为 ！=
             else if ("!=".equals(separator)) {
                 if (pair == null)
                     throw new ParseException("Illegal route rule \""
@@ -122,15 +175,18 @@ public class ConditionRouter extends AbstractRouter {
                             + content + "\".", matcher.start());
 
                 values = pair.mismatches;
+                //将content 存入到MatcherPair的 mismatches集合中
                 values.add(content);
             }
             // The Value in the KV part, if Value have more than one items.
+            //分隔符为,
             else if (",".equals(separator)) { // Should be seperateed by ','
                 if (values == null || values.isEmpty())
                     throw new ParseException("Illegal route rule \""
                             + rule + "\", The error char '" + separator
                             + "' at index " + matcher.start() + " before \""
                             + content + "\".", matcher.start());
+                // 将 content 存入到上一步获取到的 values 中，可能是 matches，也可能是 mismatches
                 values.add(content);
             } else {
                 throw new ParseException("Illegal route rule \"" + rule
@@ -141,6 +197,7 @@ public class ConditionRouter extends AbstractRouter {
         return condition;
     }
 
+    //服务路由
     @Override
     public <T> List<Invoker<T>> route(List<Invoker<T>> invokers, URL url, Invocation invocation)
             throws RpcException {
@@ -148,19 +205,32 @@ public class ConditionRouter extends AbstractRouter {
             return invokers;
         }
         try {
+            // 先对服务消费者条件进行匹配，如果匹配失败，表明服务消费者 url 不符合匹配规则，
+            // 无需进行后续匹配，直接返回 Invoker 列表即可。比如下面的规则：
+            //     host = 10.20.153.10 => host = 10.0.0.10
+            // 这条路由规则希望 IP 为 10.20.153.10 的服务消费者调用 IP 为 10.0.0.10 机器上的服务。
+            // 当消费者 ip 为 10.20.153.11 时，matchWhen 返回 false，表明当前这条路由规则不适用于
+            // 当前的服务消费者，此时无需再进行后续匹配，直接返回即可。
             if (!matchWhen(url, invocation)) {
                 return invokers;
             }
             List<Invoker<T>> result = new ArrayList<Invoker<T>>();
+            // 服务提供者匹配条件未配置，表明对指定的服务消费者禁用服务，也就是服务消费者在黑名单中
             if (thenCondition == null) {
                 logger.warn("The current consumer in the service blacklist. consumer: " + NetUtils.getLocalHost() + ", service: " + url.getServiceKey());
                 return result;
             }
+            // 这里可以简单的把 Invoker 理解为服务提供者，现在使用服务提供者匹配规则对
+            // Invoker 列表进行匹配
             for (Invoker<T> invoker : invokers) {
+                // 若匹配成功，表明当前 Invoker 符合服务提供者匹配规则。
+                // 此时将 Invoker 添加到 result 列表中
                 if (matchThen(invoker.getUrl(), url)) {
                     result.add(invoker);
                 }
             }
+            // 返回匹配结果，如果 result 为空列表，且 force = true，表示强制返回空列表，
+            // 否则路由结果为空的路由规则将自动失效
             if (!result.isEmpty()) {
                 return result;
             } else if (force) {
@@ -170,6 +240,7 @@ public class ConditionRouter extends AbstractRouter {
         } catch (Throwable t) {
             logger.error("Failed to execute condition router rule: " + getUrl() + ", invokers: " + invokers + ", cause: " + t.getMessage(), t);
         }
+        // 原样返回，此时 force = false，表示该条路由规则失效
         return invokers;
     }
 
@@ -188,36 +259,62 @@ public class ConditionRouter extends AbstractRouter {
     }
 
     boolean matchWhen(URL url, Invocation invocation) {
-        return whenCondition == null || whenCondition.isEmpty() || matchCondition(whenCondition, url, null, invocation);
+        // 服务消费者条件为 null 或空，均返回 true，比如：
+        //     => host != 172.22.3.91
+        // 表示所有的服务消费者都不得调用 IP 为 172.22.3.91 的机器上的服务
+        return whenCondition == null || whenCondition.isEmpty() ||
+                //whenCondition是服务消费者匹配条件,第二个参数url源自route方法的参数列表，该参数由外部类调用route方法时传入
+                matchCondition(whenCondition, url, null, invocation);//条件匹配
     }
 
     private boolean matchThen(URL url, URL param) {
-        return !(thenCondition == null || thenCondition.isEmpty()) && matchCondition(thenCondition, url, param, null);
+        // 服务提供者条件为 null 或空，表示禁用服务
+        return !(thenCondition == null || thenCondition.isEmpty()) &&
+                //第二个参数和第三个参数 分别是服务提供者的url 和服务消费者的url。
+                matchCondition(thenCondition, url, param, null);//进行条件匹配
     }
 
     private boolean matchCondition(Map<String, MatchPair> condition, URL url, URL param, Invocation invocation) {
+        // 将服务提供者或消费者 url 转成 Map
         Map<String, String> sample = url.toMap();
         boolean result = false;
+        // 遍历 condition 列表
         for (Map.Entry<String, MatchPair> matchPair : condition.entrySet()) {
+            // 获取匹配项名称，比如 host、method 等
             String key = matchPair.getKey();
             String sampleValue;
             //get real invoked method name from invocation
+            // 如果 invocation 不为空，且 key 为 mehtod(s)，表示进行方法匹配
             if (invocation != null && (Constants.METHOD_KEY.equals(key) || Constants.METHODS_KEY.equals(key))) {
+                // 从 invocation 获取被调用方法的名称
                 sampleValue = invocation.getMethodName();
             } else {
+                // 从服务提供者或消费者 url 中获取指定字段值，比如 host、application 等
                 sampleValue = sample.get(key);
                 if (sampleValue == null) {
+                    // 尝试通过 default.xxx 获取相应的值
                     sampleValue = sample.get(Constants.DEFAULT_KEY_PREFIX + key);
                 }
             }
+            //到这里实际上用于获取sampleValue的值
+            // --------------------✨ 分割线 ✨-------------------- //
+            //进行条件匹配
+
             if (sampleValue != null) {
+                // 调用 MatchPair 的 isMatch 方法进行匹配
                 if (!matchPair.getValue().isMatch(sampleValue, param)) {
+                    // 只要有一个规则匹配失败，立即返回 false 结束方法逻辑
                     return false;
                 } else {
                     result = true;
                 }
             } else {
                 //not pass the condition
+                // sampleValue 为空，表明服务提供者或消费者 url 中不包含相关字段。此时如果
+                // MatchPair 的 matches 不为空，表示匹配失败，返回 false。比如我们有这样
+                // 一条匹配条件 loadbalance = random，假设 url 中并不包含 loadbalance 参数，
+                // 此时 sampleValue = null。既然路由规则里限制了 loadbalance 必须为 random，
+                // 但 sampleValue = null，明显不符合规则，因此返回 false
                 if (!matchPair.getValue().matches.isEmpty()) {
                     return false;
                 } else {
@@ -228,43 +325,59 @@ public class ConditionRouter extends AbstractRouter {
         return result;
     }
 
+    //内部类  包含两个set类型的成员变量，分别用于存放匹配和不匹配的条件
     private static final class MatchPair {
         final Set<String> matches = new HashSet<String>();
         final Set<String> mismatches = new HashSet<String>();
 
+        //条件匹配
         private boolean isMatch(String value, URL param) {
+            // 情况一：matches 非空，mismatches 为空
             if (!matches.isEmpty() && mismatches.isEmpty()) {
+                // 遍历 matches 集合，检测入参 value 是否能被 matches 集合元素匹配到。
+                // 举个例子，如果 value = 10.20.153.11，matches = [10.20.153.*],
+                // 此时 isMatchGlobPattern 方法返回 true
                 for (String match : matches) {
                     if (UrlUtils.isMatchGlobPattern(match, value, param)) {
                         return true;
                     }
                 }
+                // 如果所有匹配项都无法匹配到入参，则返回 false
                 return false;
             }
-
+            // 情况二：matches 为空，mismatches 非空
             if (!mismatches.isEmpty() && matches.isEmpty()) {
                 for (String mismatch : mismatches) {
+                    // 只要入参被 mismatches 集合中的任意一个元素匹配到，就返回 false
                     if (UrlUtils.isMatchGlobPattern(mismatch, value, param)) {
                         return false;
                     }
                 }
+                // mismatches 集合中所有元素都无法匹配到入参，此时返回 true
                 return true;
             }
 
+            // 情况三：matches 非空，mismatches 非空
             if (!matches.isEmpty() && !mismatches.isEmpty()) {
                 //when both mismatches and matches contain the same value, then using mismatches first
+                // matches 和 mismatches 均为非空，此时优先使用 mismatches 集合元素对入参进行匹配。
+                // 只要 mismatches 集合中任意一个元素与入参匹配成功，就立即返回 false，结束方法逻辑
                 for (String mismatch : mismatches) {
                     if (UrlUtils.isMatchGlobPattern(mismatch, value, param)) {
                         return false;
                     }
                 }
+                // mismatches 集合元素无法匹配到入参，此时再使用 matches 继续匹配
                 for (String match : matches) {
+                    // 只要 matches 集合中任意一个元素与入参匹配成功，就立即返回 true
                     if (UrlUtils.isMatchGlobPattern(match, value, param)) {
                         return true;
                     }
                 }
+                // 全部失配，则返回 false
                 return false;
             }
+            // 情况四：matches 和 mismatches 均为空，此时返回 false
             return false;
         }
     }
