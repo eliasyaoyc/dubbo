a02e04c19fa0d99271849b31549700b9413cecf4
lancer.yao <lancer.yao@gaiaworks.cn>
2019-10-24 13:16:19 +0800

    10-24 compiler 动态编译

--- dubbo-common/src/main/java/com/alibaba/dubbo/common/compiler/support/AdaptiveCompiler.java
+++ dubbo-common/src/main/java/com/alibaba/dubbo/common/compiler/support/AdaptiveCompiler.java
@@ -36,13 +36,17 @@ public class AdaptiveCompiler implements Compiler {
     @Override
     public Class<?> compile(String code, ClassLoader classLoader) {
         Compiler compiler;
+        // 获得 Compiler 的 ExtensionLoader 对象。
         ExtensionLoader<Compiler> loader = ExtensionLoader.getExtensionLoader(Compiler.class);
         String name = DEFAULT_COMPILER; // copy reference
+        // 使用设置的拓展名，获得 Compiler 拓展对象
         if (name != null && name.length() > 0) {
             compiler = loader.getExtension(name);
+            // 获得默认的 Compiler 拓展对象
         } else {
             compiler = loader.getDefaultExtension();
         }
+        //调用真正的 Compiler 对象，动态编译代码
         return compiler.compile(code, classLoader);
     }
 
